6bd396f4c344374217869eae4f71087914193fa5
lohan008 <lohanlilva@gmail.com>
2026-06-03 18:42:30 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
 let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
 
 // 🛒 NOVA FUNÇÃO (IR PRA TELA DE TAMANHO)
-function addCarrinho(nome, preco){
-    let produto = {nome, preco};
+function addCarrinho(nome, preco,imagem){
+    let produto = {nome, preco,imagem};
 
     localStorage.setItem("produtoTemp", JSON.stringify(produto));
 
